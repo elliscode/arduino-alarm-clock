8169635c67ae6fc0fa14a64c61cc14d0e465a176
elliscode <45629640+elliscode@users.noreply.github.com>
2025-01-11 21:49:50 -0500

    Refactor and bug fix

--- arduino-alarm-clock.ino
+++ arduino-alarm-clock.ino
@@ -1,32 +1,46 @@
 #include "WiFiS3.h"
 #include "SimpleTime.h"
 #include "Arduino_LED_Matrix.h"
-#include "time_vars.h"
+
+#include "arduino_secrets.h"
 #include "songs.h"
+#include "time_vars.h"
+
 ArduinoLEDMatrix matrix;
-#include "arduino_secrets.h" 
-int alarm_times[7][2] = {{6,31},{6,1},{6,1},{6,1},{6,1},{6,1},{6,31}};
-int random_note[10];
-int random_index = 0;
-uint32_t alarm_time = 0;
-char ssid[] = SECRET_SSID;
-char pass[] = SECRET_PASS;
-char time_response[400];
-char key_i_want[] = "\"unixtime\":";
-char timezone_key[] = "\"raw_offset\":";
-uint32_t unix_time = 0;
-uint32_t time_zone = 0;
-uint32_t time_zone_negative = false;
-uint32_t time_screen[3];
+WiFiClient client;
+
+const bool LOGS_ON = false;
+const int SERVER_TIME_REFRESH_RATE = 60 * 60 * 1000; // ms
+const int ALARM_TIMEOUT = 60 * 20; // seconds
+const int alarm_times[7][2] = {{6,31},{6,1},{6,1},{6,1},{6,1},{6,1},{6,31}}; // {{hh,mm}}
+
+const char ssid[] = SECRET_SSID;
+const char pass[] = SECRET_PASS;
+const char key_i_want[] = "\"unixtime\":";
+const char timezone_key[] = "\"raw_offset\":";
+const char server[] = "worldtimeapi.org";
+const int RANDOM_NOTE_LENGTH = 10;
 
 int status = WL_IDLE_STATUS;
-char server[] = "worldtimeapi.org";
-WiFiClient client;
+int random_note[RANDOM_NOTE_LENGTH];
+int random_index = 0;
+uint32_t alarm_time = 0;
+char time_response[400];
+uint32_t unix_time = 0;
+int time_zone = 0;
+bool time_zone_negative = false;
+uint32_t time_screen[3];
+bool get_server_time = true;
+unsigned long previous_millis = 0;
+time_t current_time = 0;
+bool colon_on = true;
 
 /* -------------------------------------------------------------------------- */
 void setup() {
 /* -------------------------------------------------------------------------- */
-  // Serial.begin(9600); // uncomment this if you want logging turned on
+  if (LOGS_ON) {
+    Serial.begin(9600);
+  }
   matrix.begin();
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -53,7 +67,13 @@ void setup() {
   
   printWifiStatus();
 
-  for (int i = 0; i < 10; i++) {
+  initializeRandomNotes();
+}
+
+/* -------------------------------------------------------------------------- */
+void initializeRandomNotes() {
+/* -------------------------------------------------------------------------- */
+  for (int i = 0; i < RANDOM_NOTE_LENGTH; i++) {
     random_note[i] = random(0, sizeof(RANDOM_ARRAY)/sizeof(RANDOM_ARRAY[0]));
   }
 }
@@ -64,7 +84,6 @@ void read_response() {
   uint32_t received_data_num = 0;
   bool activate = false;
   while (client.available()) {
-    /* actual data reception */
     char c = client.read();
     if (c == '{') {
       activate = true;
@@ -75,111 +94,135 @@ void read_response() {
     }
   }  
 }
-int i;
-bool get_server_time = true;
-unsigned long previous_millis = 0;
-time_t current_time = 0;
-bool colon_on = true;
+
+/* -------------------------------------------------------------------------- */
+void getServerTime() {
+/* -------------------------------------------------------------------------- */
+  Serial.println("\nStarting connection to server...");
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("GET /api/timezone/America/New_York.json HTTP/1.1");
+    client.println("Host: worldtimeapi.org");
+    client.println("Connection: close");
+    client.println();
+  }
+  previous_millis = millis();
+
+  delay(3000);
+
+  read_response();
+
+  boolean key_valid = true;
+  boolean timezone_valid = true;
+  uint32_t key_valid_index = 0;
+  uint32_t timezone_index = 0;
+  boolean get_numbers = false;
+  boolean get_timezone = false;
+  unix_time = 0;
+  for (int i = 0; i < 1000; i++) {
+    if (time_response[i] == NULL) {
+      break;
+    }
+    if (time_response[i] == '}') {
+      Serial.println();
+    }
+
+    if (get_numbers && time_response[i] >= 48 && time_response[i] <= 57) {
+      unix_time = unix_time * 10;
+      unix_time = unix_time + (time_response[i] - 48);
+    } else {
+      get_numbers = false;
+    }
+
+    if (get_timezone && time_response[i] >= 48 && time_response[i] <= 57) {
+      time_zone = time_zone * 10;
+      time_zone = time_zone + (time_response[i] - 48);
+    } else if (get_timezone && time_response[i] == '-') {
+      time_zone_negative = !time_zone_negative;
+    } else {
+      get_timezone = false;
+    }
+
+    key_valid = key_valid && key_i_want[key_valid_index] == time_response[i];
+    if (key_valid && key_valid_index >= strlen(key_i_want) - 1) {
+      get_numbers = true;
+      Serial.println("Key found!");
+    }
+    key_valid_index++;
+
+    timezone_valid = timezone_valid && timezone_key[timezone_index] == time_response[i];
+    if (timezone_valid && timezone_index >= strlen(timezone_key) - 1) {
+      get_timezone = true;
+      Serial.println("Timezone found!");
+    }
+    timezone_index++;
+
+    Serial.print(time_response[i]);
+    if (time_response[i] == '{' || time_response[i] == ',') {
+      Serial.println();
+      Serial.print("  ");
+      key_valid = true;
+      timezone_valid = true;
+      key_valid_index = 0;
+      timezone_index = 0;
+    }
+  }
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Time found: ");
+  Serial.println(unix_time);
+
+  Serial.println();
+  Serial.print("Time zone found: ");
+  Serial.println((time_zone_negative ? -time_zone : time_zone));
+
+  Serial.println();
+  Serial.println();
+  Serial.println("disconnecting from server.");
+  client.stop();
+
+  get_server_time = false;
+}
+
+/* -------------------------------------------------------------------------- */
+void determineCurrentTime() {
+/* -------------------------------------------------------------------------- */
+  current_time = unix_time + ((millis() - previous_millis) / 1000) + (time_zone_negative ? -time_zone : time_zone);
+  Serial.print(current_time);
+  Serial.print(" -- ");
+}
+
 /* -------------------------------------------------------------------------- */
 void loop() {
 /* -------------------------------------------------------------------------- */  
-  digitalWrite(LED_BUILTIN, LOW); 
+  digitalWrite(LED_BUILTIN, LOW);
   if (get_server_time) {
-    Serial.println("\nStarting connection to server...");
-    if (client.connect(server, 80)) {
-      Serial.println("connected to server");
-      // Make a HTTP request:
-      client.println("GET /api/timezone/America/New_York.json HTTP/1.1");
-      client.println("Host: worldtimeapi.org");
-      client.println("Connection: close");
-      client.println();
-    }
-    previous_millis = millis();
-
-    delay(3000);
-
-    read_response();
-
-    boolean key_valid = true;
-    boolean timezone_valid = true;
-    uint32_t key_valid_index = 0;
-    uint32_t timezone_index = 0;
-    boolean get_numbers = false;
-    boolean get_timezone = false;
-    unix_time = 0;
-    for (i = 0; i < 1000; i++) {
-      if (time_response[i] == NULL) {
-        break;
-      }
-      if (time_response[i] == '}') {
-        Serial.println();
-      }
-
-      if (get_numbers && time_response[i] >= 48 && time_response[i] <= 57) {
-        unix_time = unix_time * 10;
-        unix_time = unix_time + (time_response[i] - 48);
-      } else {
-        get_numbers = false;
-      }
-
-      if (get_timezone && time_response[i] >= 48 && time_response[i] <= 57) {
-        time_zone = time_zone * 10;
-        time_zone = time_zone + (time_response[i] - 48);
-      } else if (get_timezone && time_response[i] == '-') {
-        time_zone_negative = !time_zone_negative;
-      } else {
-        get_timezone = false;
-      }
-
-      key_valid = key_valid && key_i_want[key_valid_index] == time_response[i];
-      if (key_valid && key_valid_index >= strlen(key_i_want) - 1) {
-        get_numbers = true;
-        Serial.println("Key found!");
-      }
-      key_valid_index++;
-
-      timezone_valid = timezone_valid && timezone_key[timezone_index] == time_response[i];
-      if (timezone_valid && timezone_index >= strlen(timezone_key) - 1) {
-        get_timezone = true;
-        Serial.println("Timezone found!");
-      }
-      timezone_index++;
-
-      Serial.print(time_response[i]);
-      if (time_response[i] == '{' || time_response[i] == ',') {
-        Serial.println();
-        Serial.print("  ");
-        key_valid = true;
-        timezone_valid = true;
-        key_valid_index = 0;
-        timezone_index = 0;
-      }
-    }
-
-    Serial.println();
-    Serial.println();
-    Serial.print("Time found: ");
-    Serial.println(unix_time);
-
-    Serial.println();
-    Serial.print("Time zone found: ");
-    Serial.println((time_zone_negative ? -time_zone : time_zone));
-
-    Serial.println();
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
-
-    get_server_time = false;
+    getServerTime();
   }
 
-  current_time = unix_time + ((millis() - previous_millis) / 1000) + (time_zone_negative ? -time_zone : time_zone);
+  determineCurrentTime();
 
   if (alarm_time == 0) {
-    determine_next_alarm();
+    initializeAlarm();
   }
 
-  while (alarm_time > 0 && alarm_time < current_time) {
+  if (alarm_time > 0 && alarm_time < current_time) {
+    playAlarm();
+  }
+
+  calculateTimeScreen();
+  matrix.loadFrame(time_screen);
+  delay(1000);
+
+  if ((millis() - previous_millis) > SERVER_TIME_REFRESH_RATE) {
+    get_server_time = true;
+  }
+}
+
+void playAlarm() {
+  while (alarm_time > 0 && alarm_time < current_time && (alarm_time + ALARM_TIMEOUT) > current_time) {
     for (int thisNote = 0; animal_crossing_notes[thisNote]!=END; thisNote++) {
       int noteToPlay = animal_crossing_notes[thisNote];
       if (noteToPlay == RANDOM) {
@@ -192,20 +235,14 @@ void loop() {
       delay(noteDuration);
       noTone(3);
     }
+    determineCurrentTime();
   }
-
-  Serial.println(current_time);
-  calculate_time_screen();
-  matrix.loadFrame(time_screen);
-  delay(1000);
-  Serial.println();
-
-  if ((millis() - previous_millis) > (60 * 60 * 1000)) {
-    get_server_time = true;
-  }
+  alarm_time = 0;
 }
 
-void calculate_time_screen() {
+/* -------------------------------------------------------------------------- */
+void calculateTimeScreen() {
+/* -------------------------------------------------------------------------- */
   int wkd = weekday(current_time) % 7;
   int hr = hour(current_time);
   if (hr == 0) {
@@ -222,6 +259,7 @@ void calculate_time_screen() {
   Serial.print(mn);
   Serial.print(':');
   Serial.print(sc);
+  Serial.println();
   for (int i = 0; i < 3; i++) {
     time_screen[i] = 0;
   }
@@ -251,7 +289,9 @@ void calculate_time_screen() {
   }
 }
 
-void determine_next_alarm() {
+/* -------------------------------------------------------------------------- */
+void initializeAlarm() {
+/* -------------------------------------------------------------------------- */
   int today = weekday(current_time) % 7;
   int tomorrow = (today + 1) % 7;
   int hr = hour(current_time);
