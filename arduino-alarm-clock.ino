712644f01e0f8206a7c732a969b10de829d5de2a
elliscode <45629640+elliscode@users.noreply.github.com>
2025-03-16 18:10:58 -0400

    The first draft of the RTC version of my alarm clock, not using the setAlarmCallback function to register the alarm, but will do that now

--- arduino-alarm-clock.ino
+++ arduino-alarm-clock.ino
@@ -1,302 +1,37 @@
 #include "WiFiS3.h"
-#include "SimpleTime.h"
 #include "Arduino_LED_Matrix.h"
-
 #include "arduino_secrets.h"
 #include "songs.h"
 #include "time_vars.h"
+#include "RTC.h"
+#include <NTPClient.h>
+#include <WiFiS3.h>
+#include <WiFiUdp.h>
+#include "arduino_secrets.h"
+#include <algorithm>
 
 ArduinoLEDMatrix matrix;
 WiFiClient client;
 
-const bool LOGS_ON = false;
-const int SERVER_TIME_REFRESH_RATE = 60 * 60 * 1000; // ms
-const int ALARM_TIMEOUT = 20 * 60; // seconds
-const int alarm_times[7][2] = {{6,30},{5,45},{5,45},{5,45},{5,45},{5,45},{6,30}}; // {{hh,mm}}
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;    // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 
-const char ssid[] = SECRET_SSID;
-const char pass[] = SECRET_PASS;
-const char key_i_want[] = "\"unixtime\":";
-const char server[] = "worldtimeapi.org";
-const int RANDOM_NOTE_LENGTH = 32;
-
-int status = WL_IDLE_STATUS;
-int random_note[RANDOM_NOTE_LENGTH];
-int random_index = 0;
-uint32_t alarm_time = 0;
-char time_response[400];
-uint32_t unix_time = 0;
-int time_zone = -14400;
-uint32_t time_screen[3];
-bool get_server_time = true;
-unsigned long previous_millis = 0;
-unsigned long previous_millis_candidate = 0;
-time_t current_time = 0;
+const bool DEBUG = false;
+const int timeZoneOffsetHours = -4; // todo get this from a API call
+RTCTime currentTime;
+RTCTime alarmTime;
+uint32_t timeScreen[3];
 bool colon_on = true;
+const int ALARM_TIMEOUT = 20 * 60 * 1000; // ms
+const int alarmTimes[7][2] = {{6,30},{5,45},{5,45},{5,45},{5,45},{5,45},{6,30}}; // {{hh,mm}}
+int random_index;
 
-/* -------------------------------------------------------------------------- */
-void setup() {
-/* -------------------------------------------------------------------------- */
-  if (LOGS_ON) {
-    Serial.begin(9600);
-  }
-  matrix.begin();
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  matrix.loadFrame(test_screen);
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    while (true);
-  }
-  
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
-  
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    status = WiFi.begin(ssid, pass);
-     
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-  
-  printWifiStatus();
+int wifiStatus = WL_IDLE_STATUS;
+WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
+NTPClient timeClient(Udp);
 
-  initializeRandomNotes();
-}
-
-/* -------------------------------------------------------------------------- */
-void loop() {
-/* -------------------------------------------------------------------------- */  
-  digitalWrite(LED_BUILTIN, LOW);
-  if (get_server_time) {
-    getServerTime();
-  }
-
-  determineCurrentTime();
-
-  if (alarm_time == 0) {
-    initializeAlarm();
-  }
-
-  if (alarm_time > 0 && alarm_time < current_time) {
-    playAlarm();
-  }
-
-  calculateTimeScreen();
-  matrix.loadFrame(time_screen);
-  delay(1000);
-
-  if ((millis() - previous_millis_candidate) > SERVER_TIME_REFRESH_RATE) {
-    get_server_time = true;
-  }
-}
-
-/* -------------------------------------------------------------------------- */
-void initializeRandomNotes() {
-/* -------------------------------------------------------------------------- */
-  for (int i = 0; i < RANDOM_NOTE_LENGTH; i++) {
-    random_note[i] = random(0, sizeof(RANDOM_ARRAY)/sizeof(RANDOM_ARRAY[0]));
-  }
-}
-
-/* -------------------------------------------------------------------------- */
-void readResponse() {
-/* -------------------------------------------------------------------------- */  
-  for (int i = 0; i < sizeof(time_response)/sizeof(time_response[0]); i++) {
-    time_response[i] = 0;
-  }
-  uint32_t received_data_num = 0;
-  bool activate = false;
-  while (client.available()) {
-    char c = client.read();
-    if (c == '{') {
-      activate = true;
-    }
-    if (activate) {
-      time_response[received_data_num] = c;
-      received_data_num++;
-    }
-  }  
-}
-
-/* -------------------------------------------------------------------------- */
-void getServerTime() {
-/* -------------------------------------------------------------------------- */
-  Serial.println("\nStarting connection to server...");
-  if (client.connect(server, 80)) {
-    Serial.println("connected to server");
-    // Make a HTTP request:
-    client.println("GET /api/timezone/UTC.json HTTP/1.1");
-    client.println("Host: worldtimeapi.org");
-    client.println("Connection: close");
-    client.println();
-  }
-  previous_millis_candidate = millis();
-
-  delay(3000);
-
-  readResponse();
-
-  boolean key_valid = true;
-  uint32_t key_valid_index = 0;
-  boolean get_numbers = false;
-  boolean data_found = false;
-  for (int i = 0; i < 1000; i++) {
-    if (time_response[i] == NULL) {
-      break;
-    }
-    if (time_response[i] == '}') {
-      Serial.println();
-    }
-
-    if (get_numbers && time_response[i] >= 48 && time_response[i] <= 57) {
-      unix_time = unix_time * 10;
-      unix_time = unix_time + (time_response[i] - 48);
-    } else {
-      get_numbers = false;
-    }
-
-    key_valid = key_valid && key_i_want[key_valid_index] == time_response[i];
-    if (key_valid && key_valid_index >= strlen(key_i_want) - 1) {
-      get_numbers = true;
-      Serial.println("Key found!");
-      unix_time = 0;
-      previous_millis = previous_millis_candidate;
-      data_found = true;
-    }
-    key_valid_index++;
-
-    Serial.print(time_response[i]);
-    if (time_response[i] == '{' || time_response[i] == ',') {
-      Serial.println();
-      Serial.print("  ");
-      key_valid = true;
-      key_valid_index = 0;
-    }
-  }
-
-  if (data_found) {
-    Serial.println();
-    Serial.println();
-    Serial.print("Time found: ");
-    Serial.println(unix_time);
-
-    Serial.println();
-    Serial.print("Time zone found: ");
-    Serial.println(time_zone);
-  }
-
-  Serial.println();
-  Serial.println();
-  Serial.println("disconnecting from server.");
-  client.stop();
-
-  get_server_time = false;
-}
-
-/* -------------------------------------------------------------------------- */
-void determineCurrentTime() {
-/* -------------------------------------------------------------------------- */
-  current_time = unix_time + ((millis() - previous_millis) / 1000) + time_zone;
-  Serial.print(current_time);
-  Serial.print(" -- ");
-}
-
-/* -------------------------------------------------------------------------- */
-void playAlarm() {
-/* -------------------------------------------------------------------------- */
-  while (alarm_time > 0 && alarm_time < current_time && (alarm_time + ALARM_TIMEOUT) > current_time) {
-    for (int thisNote = 0; animal_crossing_notes[thisNote]!=END; thisNote++) {
-      int noteToPlay = animal_crossing_notes[thisNote];
-      if (noteToPlay == RANDOM) {
-        noteToPlay = RANDOM_ARRAY[random_note[random_index]];
-        random_index = (random_index + 1) % 10;
-      }
-      int noteDuration = animal_crossing_speed*animal_crossing_times[thisNote];
-      tone(3, noteToPlay,noteDuration*.95);
-      Serial.println(noteToPlay);
-      delay(noteDuration);
-      noTone(3);
-    }
-    determineCurrentTime();
-  }
-  alarm_time = 0;
-}
-
-/* -------------------------------------------------------------------------- */
-void calculateTimeScreen() {
-/* -------------------------------------------------------------------------- */
-  int wkd = weekday(current_time) % 7;
-  int hr = hour(current_time);
-  if (hr == 0) {
-    hr = 12;
-  } else if (hr > 12) {
-    hr = hr - 12;
-  }
-  int mn = minute(current_time);
-  int sc = second(current_time);
-  Serial.print(wkd);
-  Serial.print(" -- ");
-  Serial.print(hr);
-  Serial.print(':');
-  Serial.print(mn);
-  Serial.print(':');
-  Serial.print(sc);
-  Serial.println();
-  for (int i = 0; i < 3; i++) {
-    time_screen[i] = 0;
-  }
-
-  if (hr >= 10) {
-    for (int i = 0; i < 3; i++) {
-      time_screen[i] = time_screen[i] | hour_tens[i];
-    }
-  }
-  for (int i = 0; i < 3; i++) {
-    time_screen[i] = time_screen[i] | hour_ones[(hr % 10)][i];
-  }
-
-  if (colon_on) {
-    for (int i = 0; i < 3; i++) {
-      time_screen[i] = time_screen[i] | colon[i];
-    }
-  }
-
-  colon_on = !colon_on;
-
-  for (int i = 0; i < 3; i++) {
-    time_screen[i] = time_screen[i] | minute_tens[(mn / 10)][i];
-  }
-  for (int i = 0; i < 3; i++) {
-    time_screen[i] = time_screen[i] | minute_ones[(mn % 10)][i];
-  }
-}
-
-/* -------------------------------------------------------------------------- */
-void initializeAlarm() {
-/* -------------------------------------------------------------------------- */
-  int today = weekday(current_time) % 7;
-  int tomorrow = (today + 1) % 7;
-  int hr = hour(current_time);
-  int mn = minute(current_time);
-
-  if (hr < alarm_times[today][0] || (hr == alarm_times[today][0] && mn < alarm_times[today][1])) {
-    alarm_time = current_time - (current_time % 60) + ((alarm_times[today][1] - mn) * 60) + ((alarm_times[today][0] - hr) * 60 * 60);
-  } else {
-    alarm_time = current_time - (current_time % 60) + ((alarm_times[tomorrow][1] - mn) * 60) + ((alarm_times[tomorrow][0] - hr) * 60 * 60) + (24 * 60 * 60);
-  }
-  Serial.print("Alarm time: ");
-  Serial.println(alarm_time);
-}
-
-/* -------------------------------------------------------------------------- */
 void printWifiStatus() {
-/* -------------------------------------------------------------------------- */
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
@@ -311,4 +46,160 @@ void printWifiStatus() {
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
+}
+
+void connectToWiFi(){
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (wifiStatus != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    wifiStatus = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  Serial.println("Connected to WiFi");
+  printWifiStatus();
+}
+
+void setup(){
+  if (DEBUG) {
+    Serial.begin(9600);
+    while (!Serial);
+  }
+  Serial.println("Communication with serial port succeeded!");
+  matrix.begin();
+  matrix.loadFrame(test_screen);
+
+  connectToWiFi();
+  RTC.begin();
+  Serial.println("\nStarting connection to server...");
+
+  timeClient.begin();
+  while (!timeClient.isTimeSet()) {
+    Serial.println("timeClient not set yet, waiting 5 second before attempting...");
+    delay(5000);
+    timeClient.update();
+  }
+
+  unsigned long unixTime = timeClient.getEpochTime() + (timeZoneOffsetHours * 3600);
+  Serial.print("Unix time = ");
+  Serial.println(unixTime);
+  RTCTime timeToSet = RTCTime(unixTime);
+  RTC.setTime(timeToSet);
+
+  // Retrieve the date and time from the RTC and print them
+  RTC.getTime(currentTime); 
+  Serial.println("The RTC was just set to: " + String(currentTime));
+
+  calculateNextAlarmTime();
+}
+
+void calculateNextAlarmTime() {
+  alarmTime = RTCTime(currentTime);
+  alarmTime.setHour(0);
+  alarmTime.setMinute(0);
+  alarmTime.setSecond(0);
+  int dayOfWeekIndex = (int) currentTime.getDayOfWeek();
+  int nextAlarm[] = {alarmTimes[dayOfWeekIndex][0], alarmTimes[dayOfWeekIndex][1]};
+  bool todaysAlarmAlreadyPassed = currentTime.getHour() > nextAlarm[0] || (currentTime.getHour() == nextAlarm[0] && currentTime.getMinutes() >= nextAlarm[1]);
+  if (todaysAlarmAlreadyPassed) {
+    alarmTime.setUnixTime(alarmTime.getUnixTime() + 86400);
+    dayOfWeekIndex = (dayOfWeekIndex + 1) % 7;
+    nextAlarm[0] = alarmTimes[dayOfWeekIndex][0];
+    nextAlarm[1] = alarmTimes[dayOfWeekIndex][1];
+  }
+  alarmTime.setHour(nextAlarm[0]);
+  alarmTime.setMinute(nextAlarm[1]);
+  Serial.println("The alarm was just set to: " + String(alarmTime));
+}
+
+void loop(){
+  if (alarmTime.getUnixTime() <= currentTime.getUnixTime()) {
+    playAlarm();
+    calculateNextAlarmTime();
+  }
+  calculateTimeScreen();
+  matrix.loadFrame(timeScreen);
+  delay(1000);
+}
+
+void calculateTimeScreen() {
+  RTC.getTime(currentTime);
+  int hr = currentTime.getHour();
+  if (hr > 12) {
+    hr = hr - 12;
+  } else if (hr == 0) {
+    hr = 12;
+  }
+  int mn = currentTime.getMinutes();
+  Serial.print((int) currentTime.getDayOfWeek());
+  Serial.print(" -- ");
+  Serial.print(hr);
+  Serial.print(':');
+  Serial.print(mn);
+  Serial.print(':');
+  Serial.print(currentTime.getSeconds());
+  Serial.println();
+  for (int i = 0; i < 3; i++) {
+    timeScreen[i] = 0;
+  }
+
+  if (hr >= 10) {
+    for (int i = 0; i < 3; i++) {
+      timeScreen[i] = timeScreen[i] | hour_tens[i];
+    }
+  }
+  for (int i = 0; i < 3; i++) {
+    timeScreen[i] = timeScreen[i] | hour_ones[(hr % 10)][i];
+  }
+
+  if (colon_on) {
+    for (int i = 0; i < 3; i++) {
+      timeScreen[i] = timeScreen[i] | colon[i];
+    }
+  }
+
+  colon_on = !colon_on;
+
+  for (int i = 0; i < 3; i++) {
+    timeScreen[i] = timeScreen[i] | minute_tens[(mn / 10)][i];
+  }
+  for (int i = 0; i < 3; i++) {
+    timeScreen[i] = timeScreen[i] | minute_ones[(mn % 10)][i];
+  }
+}
+
+void playAlarm() {
+  int happyFaceIndex = 0;
+  unsigned long alarmMiillis = millis();
+  unsigned long previousTimeTicked = millis();
+  while (alarmMiillis > 0 && alarmMiillis <= millis() && (alarmMiillis + ALARM_TIMEOUT) >= millis()) {
+    for (int thisNote = 0; animal_crossing_notes[thisNote]!=END; thisNote++) {
+      int noteToPlay = animal_crossing_notes[thisNote];
+      int noteDuration = animal_crossing_speed*animal_crossing_times[thisNote];
+      tone(3, noteToPlay,noteDuration*.95);
+      delay(noteDuration);
+      noTone(3);
+      if (millis() - 1000 > previousTimeTicked) {
+        matrix.loadFrame(happy_face[happyFaceIndex]);
+        happyFaceIndex = (happyFaceIndex + 1) % 4;
+        previousTimeTicked = millis();
+      }
+    }
+  }
 }
